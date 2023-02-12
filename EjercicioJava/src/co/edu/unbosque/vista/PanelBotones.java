1bf7194cb5c3b6417a8de06b24d5b5637d16bdaf
andresk2003 <124805588+andresk2003@users.noreply.github.com>
2023-02-12 18:52:57 -0500

    Update PanelBotones.java

--- EjercicioJava/src/co/edu/unbosque/vista/PanelBotones.java
+++ EjercicioJava/src/co/edu/unbosque/vista/PanelBotones.java
@@ -1,5 +1,54 @@
 package co.edu.unbosque.vista;
 
-public class PanelBotones {
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class PanelBotones extends JPanel{
+    
+    private static final long serialVersionUID = 8877347642037037045L;
+	private JButton btnAgregar;
+    private JButton btnEliminar;
+    private JButton btnVer;
+    
+    public PanelBotones(){
+        setLayout(new GridLayout(3, 1, 8, 8));
+        inicializarComponentes();
+        setVisible(true);
+    }
+    
+    public void inicializarComponentes(){
+        
+        btnAgregar = new JButton();
+        btnAgregar.setText("Agregar Candidato");
+        btnAgregar.setActionCommand("AGREGAR");
+
+        btnEliminar = new JButton();
+        btnEliminar.setText("Eliminar Candidato");
+        btnEliminar.setActionCommand("ELIMINAR");
+
+        btnVer = new JButton();
+        btnVer.setText("Ver Candidato");
+        btnVer.setActionCommand("VER");
+        
+        add(btnAgregar);
+        add(btnEliminar);
+        add(btnVer);
+        
+    }
+
+    public JButton getBtnAgregar() {
+        return btnAgregar;
+    }
+
+    public JButton getBtnEliminar() {
+        return btnEliminar;
+    }
+
+    public JButton getBtnVer() {
+        return btnVer;
+    }
+    
+    
 }
