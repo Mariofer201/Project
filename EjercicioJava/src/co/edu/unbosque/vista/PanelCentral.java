31d12ceeb2442e3b1d2901ad3f622092c95f3cea
andresk2003 <124805588+andresk2003@users.noreply.github.com>
2023-02-12 18:53:17 -0500

    Update PanelCentral.java

--- EjercicioJava/src/co/edu/unbosque/vista/PanelCentral.java
+++ EjercicioJava/src/co/edu/unbosque/vista/PanelCentral.java
@@ -1,5 +1,39 @@
 package co.edu.unbosque.vista;
 
-public class PanelCentral {
+import java.awt.GridLayout;
 
+import javax.swing.JPanel;
+
+public class PanelCentral extends JPanel {
+
+	private static final long serialVersionUID = -3058875128977026240L;
+	private PanelDatos panelDatos;
+	private PanelBotones panelBotones;
+
+	public PanelCentral() {
+		setLayout(new GridLayout(1, 2, 15, 15));
+		inicializarComponentes();
+		setVisible(true);
+	}
+
+	public void inicializarComponentes() {
+
+		// PANEL INFORMACION
+		panelDatos = new PanelDatos();
+
+		// PANEL BOTONES
+		panelBotones = new PanelBotones();
+
+		add(panelDatos);
+		add(panelBotones);
+
+	}
+
+	public PanelDatos getPanelDatos() {
+		return panelDatos;
+	}
+
+	public PanelBotones getPanelBotones() {
+		return panelBotones;
+	}
 }
