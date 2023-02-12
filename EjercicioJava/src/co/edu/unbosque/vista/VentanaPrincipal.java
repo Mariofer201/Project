eb47bd9f647036360a3d51a37530d1ed4c8c7951
andresk2003 <124805588+andresk2003@users.noreply.github.com>
2023-02-12 18:54:04 -0500

    Update VentanaPrincipal.java

--- EjercicioJava/src/co/edu/unbosque/vista/VentanaPrincipal.java
+++ EjercicioJava/src/co/edu/unbosque/vista/VentanaPrincipal.java
@@ -1,5 +1,52 @@
 package co.edu.unbosque.vista;
 
-public class VentanaPrincipal {
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class VentanaPrincipal extends JFrame {
+
+	private static final long serialVersionUID = -6769638857830900014L;
+	private PanelCentral panelCentral;
+
+	public VentanaPrincipal() {
+		setTitle("Crud canddatos");
+		setSize(800, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		inicializarComponentes();
+
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+
+	public void inicializarComponentes() {
+
+		getContentPane().setLayout(new BorderLayout());
+
+		panelCentral = new PanelCentral();
+
+		getContentPane().add(panelCentral, BorderLayout.CENTER);
+
+	}
+
+	public PanelCentral getPanelCentral() {
+		return panelCentral;
+	}
+
+	public void mostrarMensaje(String mensaje, String titulo) {
+		if(titulo.equals("ERROR")) {
+			JOptionPane.showMessageDialog(null, mensaje, titulo, JOptionPane.ERROR_MESSAGE);
+		}else {
+			JOptionPane.showMessageDialog(null, mensaje, titulo, JOptionPane.INFORMATION_MESSAGE);
+		}
+		
+	}
+	
+	public String capturarDato(String mensaje, String titulo) {
+		return JOptionPane.showInputDialog(null,mensaje,titulo,JOptionPane.QUESTION_MESSAGE);
+	}
 
 }
